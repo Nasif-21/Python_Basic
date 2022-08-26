eb661497a163672653f7c83722cc9f420c0c4542
Nasif-21 <77929303+Nasif-21@users.noreply.github.com>
2022-08-26 22:05:56 +0600

    Class And Object
    Class and Object things in Python

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-name=["Sheikh","Muhtasim","Nasif"]
-print(name)
-print(name[0])
-print(len(name)) #length of array
+class Myself:        #Defining a class
+    def name(self):  #Inside class, we call a function
+        print("I am Nasif")  #Printing inside a class
 
-for n in name:    #array looping
-    print(name)
+myself=Myself()           #Creating an Object
+myself.name()
 
-name.append("Shitab") #adding an element
-print(name)
+class Sum():
 
-name.pop(3) #remove a data using index number
-print(name)
+      print("Enter number 1")
+      a=int(input())
+      print("Enter number 2")
+      b=int(input())
 
-name.remove("Muhtasim")
-print(name)
+      print("Sum =",a+b)
 
-number=[20,60,33,48,96]
-print(number.count(20)) #specifing individual index
-number.reverse()
-print(number)
-number.sort()
-print(number)
-name.extend(number)  #Adding 2 array elements
-print(name)
-
+a=Sum()
