72666e73c517ccdf2ed3c538d5556d32310a42be
Nasif-21 <77929303+Nasif-21@users.noreply.github.com>
2022-09-02 22:20:30 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-name=["Sheikh","Muhtasim","Nasif"]
-print(name)
-print(name[0])
-print(len(name)) #length of array
+import json
 
-for n in name:    #array looping
-    print(name)
+a='{"Name":"Nasif", "Education":"CSE"}'
+jsload=json.loads(a)
+print(jsload["Name"]) #Reading a index data from Json
+print(jsload["Education"])
 
-name.append("Shitab") #adding an element
-print(name)
+#Json Dump
 
-name.pop(3) #remove a data using index number
-print(name)
-
-name.remove("Muhtasim")
-print(name)
-
-number=[20,60,33,48,96]
-print(number.count(20)) #specifing individual index
-number.reverse()
-print(number)
-number.sort()
-print(number)
-name.extend(number)  #Adding 2 array elements
-print(name)
-
+Details={
+    "Name":"Mr K",
+    "Age": "30",
+    "Adress":"DOHS"
+}
+dump=json.dumps(Details) #Dumping Json Data
+print(dump)
