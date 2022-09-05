068a740e5c8ea82ad4e5e703d37b8731a8408f0b
Nasif-21 <77929303+Nasif-21@users.noreply.github.com>
2022-09-05 22:57:48 +0600

    File read write
    variable_name=open("file name","r") to read a file
    
    variable _name=open("file name","a/w")
    variable_name.write("") to write inside a file

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-name=["Sheikh","Muhtasim","Nasif"]
-print(name)
-print(name[0])
-print(len(name)) #length of array
+newfile=open("E:\Python Basics\Filework\Secret.txt") # opening a file for reading
+print(newfile.read())
+newfile.close()
 
-for n in name:    #array looping
-    print(name)
+newfile2=open("E:\Python Basics\Filework\Laboh ko.txt.txt","a") #using append, a file is created
+newfile2.write("Labon Ko Labon Pe Sajao")
+newfile2.write("Kya Ho Tum Mujhe Ab Batao")
+newfile2.write("Todh Do Khud Ko Tum")
+newfile2.write("Banhon Mein Meri *3 ")
 
-name.append("Shitab") #adding an element
-print(name)
-
-name.pop(3) #remove a data using index number
-print(name)
-
-name.remove("Muhtasim")
-print(name)
-
-number=[20,60,33,48,96]
-print(number.count(20)) #specifing individual index
-number.reverse()
-print(number)
-number.sort()
-print(number)
-name.extend(number)  #Adding 2 array elements
-print(name)
-
+newfile2.close()
+newfile2=open("E:\Python Basics\Filework\Laboh ko.txt.txt","r")
+print(newfile2.readline())
+print(newfile2.readline())
+print("Not a good option though")
+newfile2=open("E:\Python Basics\Filework\Laboh ko.txt.txt","r") #To read line by line
+for song in newfile2:
+    print(song)
